8ab9f6578c44223fd7ff321234acee52fbd3ecb4
rubenmt <rmatiastiscar@safareyes.es>
2022-04-19 20:48:09 +0200

    tabla aforo

--- src/test/java/UtilidadesCamarero/Aforo.java
+++ src/test/java/UtilidadesCamarero/Aforo.java
@@ -1,58 +1,70 @@
 package UtilidadesCamarero;
 
+import Modelos.Mesa;
+import UtilidadesBBDD.ObtenerMesas;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 
 class Aforo extends JFrame {
-    public Aforo(){
 
-        JPanel panelExterno = new JPanel(new GridLayout(10,10,10,10));
-        panelExterno.setOpaque(false);
-        panelExterno.setBorder(BorderFactory.createEmptyBorder(20,0,0,0));
 
-        JLabel labelMesa1 = new JLabel("Mesa");
-        JTextField textoMesa1 = new JTextField();
-        JButton botonLibre = new JButton("Liberar");
-        JButton botonOcupado = new JButton("Ocupar");
+        private JPanel topPanel;
+        private JTable table;
+        private JScrollPane scrollPane;
+        private String[] columns = new String[3];
+        private String[][] data = new String[3][3];
+        JButton button = new JButton();
+  public Aforo()
+        {
+            setTitle("JButton in JTable");
+            setSize(300,150);
+            topPanel = new JPanel();
+            topPanel.setLayout(new BorderLayout());
+            getContentPane().add(topPanel);
+            columns = new String[] {"Id",  "Action"};
+            data = new String[][]{
+            };
+            DefaultTableModel model = new DefaultTableModel(data,columns);
+            table = new JTable();
+            table.setModel(model);
+            //table.getColumn("Action").setCellRenderer((TableCellRenderer) new ButtonRenderer());
+            JComboBox combo = new JComboBox();
+            combo.addItem("Libre");
+            combo.addItem("Ocupada");
 
-        botonLibre.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textoMesa1.setBackground(Color.green);
+            table.getColumn("Action").setCellEditor(new DefaultCellEditor(combo));
+            scrollPane = new JScrollPane(table);
+            topPanel.add(scrollPane,BorderLayout.CENTER);
+
+            List<Mesa> listaMesas = ObtenerMesas.obtenerMesas();
+
+            for (Mesa m : listaMesas){
+                model.addRow(new Object[]{m.getNumeroMesa()});
             }
-        } );
 
-        botonOcupado.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                textoMesa1.setBackground(Color.RED);
-            }
-        } );
+        }
 
 
-        setBounds(500,50,600,900);
 
-        panelExterno.add(labelMesa1);
-        panelExterno.add(textoMesa1);
-        panelExterno.add(botonLibre);
-        panelExterno.add(botonOcupado);
-        add(panelExterno);
 
-    }
-
-    public static void main(String[] args) {
-        Aforo a1 = new Aforo();
-        a1.setVisible(true);
-        a1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        a1.setSize(400,400);
-    }
+        public static void main(String args[])
+        {
+            Aforo f = new Aforo();
+            f.setVisible(true);
+        }
 
 
 }
