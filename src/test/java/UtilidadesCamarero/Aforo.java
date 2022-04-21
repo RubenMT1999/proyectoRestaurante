6b4130fedaa49d8ac5c3e97e764a950228789396
rubenmt <rmatiastiscar@safareyes.es>
2022-04-21 20:47:07 +0200

    tabla aforo

--- src/test/java/UtilidadesCamarero/Aforo.java
+++ src/test/java/UtilidadesCamarero/Aforo.java
@@ -21,39 +21,24 @@ import java.util.List;
 class Aforo extends JFrame {
 
 
-        private JPanel topPanel;
-        private JTable table;
-        private JScrollPane scrollPane;
-        private String[] columns = new String[3];
-        private String[][] data = new String[3][3];
-        JButton button = new JButton();
+
   public Aforo()
         {
             setTitle("JButton in JTable");
-            setSize(300,150);
-            topPanel = new JPanel();
-            topPanel.setLayout(new BorderLayout());
-            getContentPane().add(topPanel);
-            columns = new String[] {"Id",  "Action"};
-            data = new String[][]{
-            };
-            DefaultTableModel model = new DefaultTableModel(data,columns);
-            table = new JTable();
-            table.setModel(model);
-            //table.getColumn("Action").setCellRenderer((TableCellRenderer) new ButtonRenderer());
-            JComboBox combo = new JComboBox();
-            combo.addItem("Libre");
-            combo.addItem("Ocupada");
+            setSize(100,150);
+            JPanel panelExterno = new JPanel(new GridLayout(0,2,10,10));
 
-            table.getColumn("Action").setCellEditor(new DefaultCellEditor(combo));
-            scrollPane = new JScrollPane(table);
-            topPanel.add(scrollPane,BorderLayout.CENTER);
 
             List<Mesa> listaMesas = ObtenerMesas.obtenerMesas();
 
             for (Mesa m : listaMesas){
-                model.addRow(new Object[]{m.getNumeroMesa()});
+                JLabel label1 = new JLabel(String.valueOf(m.getNumeroMesa()));
+                Checkbox check1 = new Checkbox();
+                panelExterno.add(label1);
+                panelExterno.add(check1);
+
             }
+            add(panelExterno);
 
         }
 
