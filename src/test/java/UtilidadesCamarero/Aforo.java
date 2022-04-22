01dbd7e893bce63e9b6994ca128c995e8fb90268
rubenmt <rmatiastiscar@safareyes.es>
2022-04-22 17:06:32 +0200

    aforo terminado

--- src/test/java/UtilidadesCamarero/Aforo.java
+++ src/test/java/UtilidadesCamarero/Aforo.java
@@ -15,30 +15,96 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
+
+import static UtilidadesBBDD.UtilidadesBBDD.cerrarConexion;
+import static UtilidadesBBDD.UtilidadesBBDD.conectarConBD;
 
 
 class Aforo extends JFrame {
 
+    public static  int contador;
 
 
   public Aforo()
         {
             setTitle("JButton in JTable");
-            setSize(100,150);
-            JPanel panelExterno = new JPanel(new GridLayout(0,2,10,10));
 
+            JPanel panelExterno = new JPanel(new GridLayout(0,2,50,30));
+            panelExterno.setBorder(BorderFactory.createEmptyBorder(10,10,10,30));
 
             List<Mesa> listaMesas = ObtenerMesas.obtenerMesas();
 
+            JLabel labelMesas = new JLabel("      "+"Nº Mesa");
+            JLabel labelEstado = new JLabel("      "+"Estado");
+            panelExterno.add(labelMesas);
+            panelExterno.add(labelEstado);
+
+            JButton boton1 = new JButton("Aceptar");
+            JLabel labelVacio = new JLabel("");
+
+            contador = listaMesas.size();
+
+
             for (Mesa m : listaMesas){
-                JLabel label1 = new JLabel(String.valueOf(m.getNumeroMesa()));
-                Checkbox check1 = new Checkbox();
+                JLabel label1 = new JLabel("           "+String.valueOf(m.getNumeroMesa()));
+                JComboBox combo1 = new JComboBox();
+                combo1.addItem("Ocupado");
+                combo1.addItem("Libre");
+
+
+                if (m.getLibre()==1){
+                    combo1.setSelectedItem("Libre");
+                }else {
+                    combo1.setSelectedItem("Ocupado");
+                }
+
                 panelExterno.add(label1);
-                panelExterno.add(check1);
+                panelExterno.add(combo1);
+
+
+                boton1.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        int eleccion = combo1.getSelectedIndex();
+
+                        Connection con = conectarConBD();
+
+                        try{
+                            PreparedStatement query = con.prepareStatement("UPDATE mesa set libre = "
+                                    +String.valueOf(eleccion)+
+                                    " where numero_mesa ="+String.valueOf(listaMesas.get(contador-1).getNumeroMesa())
+                            );
+                            contador--;
+                            ResultSet rs = query.executeQuery();
+
+                            if (contador == 0){
+                                contador= listaMesas.size();
+                                JOptionPane.showMessageDialog(panelExterno,
+                                        "Mesas actualizadas correctamente");
+                            }
+
+                        }catch (Exception u){
+                            System.out.println(u);
+                        }finally {
+                            cerrarConexion(con);
+                        }
+
+                    }
+                });
 
             }
+
+
+            panelExterno.add(labelVacio);
+            panelExterno.add(boton1);
+
             add(panelExterno);
+            setBounds(500,50,300,400);
 
         }
 
@@ -49,6 +115,7 @@ class Aforo extends JFrame {
         {
             Aforo f = new Aforo();
             f.setVisible(true);
+            f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         }
 
 
